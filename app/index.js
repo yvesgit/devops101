e5d4abd6f2ebfa82547d7da697771c81444ec892
Yves Borenstein <yvesborenstein@hotmail.com>
2021-09-25 12:23:37 +0800

    first commit

---
+++ app/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const port = 3000
+const app = express()
+app.get('/', (req, res) => {
+res.send('Hello World!')
+})
+app.listen(port, (portport) => {
+console.log(`Example app listening at http://localhost:${port}`)
+})
